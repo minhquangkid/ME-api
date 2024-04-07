a4c24c121dee2b444ff4a3c22fd94fe056bcffd7
quang <minhquangkid@gmail.com>
2024-04-07 22:33:35 +0700

    add database

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ME_api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ME_api.Models
+{
+    public class MainCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; } // không có required ở đây cũng được vì đã có Required ờ định nghĩa trên đầu rồi
+    }
+}
